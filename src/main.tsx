1fbfd3fbb069fafa912f01a62b2e5a19df99a0a3
seanplus73-a11y <seanplus73@gmail.com>
2026-02-23 12:15:35 -0700

    / Force fresh build - 2025-02-23

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './styles/index.css';
 import App from './app/App';
 
+// Force fresh build - 2025-02-23
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
