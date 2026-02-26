c7a873f9ca3be6bc19ceec64ce1485dca17cbf67
seanplus73-a11y <seanplus73@gmail.com>
2026-02-25 22:37:01 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,7 @@ export default defineConfig({
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
+  
+  // Ensure _routes.json is copied to dist
+  publicDir: 'public',
 })
