898dd9b28547868c804442b2c6bade5456175c6d
seanplus73-a11y <seanplus73@gmail.com>
2026-04-21 16:40:40 -0700

    Update routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+import { createBrowserRouter, Outlet, Navigate } from "react-router";
 import Navbar from "./components/Navbar";
 import { Footer } from "./components/Footer";
 
