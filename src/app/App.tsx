d2783848182f2795e3ec420787a8cf5a4dd68cbf
seanplus73-a11y <seanplus73@gmail.com>
2026-04-21 13:48:48 -0700

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,6 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
-import DriveCarToMechanic from "./pages/DriveCarToMechanic";
 
-<Route path="/drive-car-to-mechanic" element={<DriveCarToMechanic />} />
 export default function App() {
   return <RouterProvider router={router} />;
 }
