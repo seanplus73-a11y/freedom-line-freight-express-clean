e825d2ad06356e0a5a21d1269a8f5cdd432bd8d4
seanplus73-a11y <seanplus73@gmail.com>
2026-04-21 15:03:17 -0700

    Update routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -31,6 +31,7 @@ import CasaGrandeAutoPartsDelivery from "./pages/CasaGrandeAutoPartsDelivery";
 import DriveCarToMechanic from "./pages/DriveCarToMechanic";
 import CarDeliveryService from "./pages/CarDeliveryService";
 import DriveMyCarHome from "./pages/DriveMyCarHome";
+import SomeoneToDriveMyCar from "./pages/SomeoneToDriveMyCar";
 function RootLayout() {
   return (
     <div className="min-h-screen bg-[#111827] text-white">
@@ -83,6 +84,7 @@ export const router = createBrowserRouter([
       { path: "san-tan-valley-vehicle-delivery", element: <Navigate to="/san-tan-valley-delivery" replace /> },
       { path: "car-delivery-service", element: <CarDeliveryService /> },
       { path: "drive-my-car-home", element: <DriveMyCarHome /> },
+      { path: "someone-to-drive-my-car", element: <SomeoneToDriveMyCar /> },
     ],
   },
 ]);
