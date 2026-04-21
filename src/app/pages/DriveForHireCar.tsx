fe69bf817902942a9b3e150e96f8d1d2ec300b84
seanplus73-a11y <seanplus73@gmail.com>
2026-04-21 16:43:27 -0700

    Update DriveForHireCar.tsx

--- src/app/pages/DriveForHireCar.tsx
+++ src/app/pages/DriveForHireCar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import { UserCheck, Car, MapPin, ShieldCheck, CheckCircle } from "lucide-react";
 
 export default function DriveForHireCar() {
