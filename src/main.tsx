016ca8508042cb7440b7f645651ddaaad07bc38a
seanplus73-a11y <seanplus73@gmail.com>
2026-04-20 22:31:43 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./app/App";
-import "./index.css";
+import "./app/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
