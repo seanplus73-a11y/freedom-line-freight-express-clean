a89eb2f85997f895e7d2ce1ace765fd360a317cf
seanplus73-a11y <seanplus73@gmail.com>
2026-04-21 00:24:43 -0700

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
-import { RouterProvider } from "react-router";
+import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
-import { Toaster } from "./components/ui/sonner";
+import { Toaster } from "../components/ui/sonner";
 
 export default function App() {
   return (
