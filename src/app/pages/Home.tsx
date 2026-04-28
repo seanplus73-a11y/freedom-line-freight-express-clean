43a089407981dfab8f20d730c3696e6776ba2852
seanplus73-a11y <seanplus73@gmail.com>
2026-04-27 23:25:12 -0700

    Add link to contact page in Home component

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -478,6 +478,7 @@ export default function Home() {
             </p>
 
             <div className="flex flex-col sm:flex-row gap-4 justify-center">
+              
               <Link
   to="/contact"
   className="inline-flex items-center justify-center bg-white text-orange-600 px-10 py-4 rounded-md font-bold text-lg hover:bg-gray-100 transition-colors"
