46f0922d9eb9f7e89f4c91e627c9663b9df322a0
seanplus73-a11y <seanplus73@gmail.com>
2026-05-02 14:40:57 -0700

    Remove BlogPost route from routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -52,7 +52,7 @@ import ArizonaDeliveryGuide from "./pages/ArizonaDeliveryGuide";
 
 // Blog
 import Blog from "./pages/Blog";
-import BlogPost from "./pages/BlogPost";
+
 
 function RootLayout() {
   return (
@@ -126,7 +126,7 @@ export const router = createBrowserRouter([
 
       // Blog routes
       { path: "blog", element: <Blog /> },
-      { path: "blog/:slug", element: <BlogPost /> },
+      
     ],
   },
 ]);
