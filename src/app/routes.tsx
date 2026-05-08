5eab15c6aa1d0d9cb75cb605bb50e4f146971d5b
seanplus73-a11y <seanplus73@gmail.com>
2026-05-07 20:06:37 -0700

    Refactor oil change route definition

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -187,10 +187,7 @@ export const router = createBrowserRouter([
       { path: "vehicle-concierge",element: <VehicleConcierge />,},
       { path: "vehicle-concierge-phoenix", element: <VehicleConciergePhoenix /> },
       { path: "car-wash-pickup-service-phoenix", element: <CarWashPickupServicePhoenix /> }
-      {
-  path: "/oil-change-pickup-service-phoenix",
-  element: <OilChangePickupServicePhoenix />,
-},
+      { path: "/oil-change-pickup-service-phoenix", element: <OilChangePickupServicePhoenix />, },
 
 
 
